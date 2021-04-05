275e416cadc06c116286452b2eb48f6bb5f01835
Bri <brigitta.rucz@yahoo.com>
2021-04-05 16:04:42 +0200

    auth controller postAuth

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,3 +26,13 @@ exports.getAuth = (req, res, next) => {
 
     res.render('auth');
 }
+
+exports.postAuth = (req, res, next) => {
+    const username = req.body.username;
+    if(username === "root") {
+        return res.redirect('/dashboard/' + username);
+    } else {
+        console.log(new Error("Invalid username"))
+        return res.redirect('/');
+    }
+}
